9f6ba5a83a03e426db713b08491671920a053188
borjagm <borjagm08@gmail.com>
2025-03-03 13:55:57 +0100

    [FEAT-3] Limpieza de código

--- src/context/HeroesContext.jsx
+++ src/context/HeroesContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useEffect } from 'react';
 import { fetchData } from '@services/ApiServices';
 import PropTypes from 'prop-types';
-import { getHeroesUrl, getHeroDetailUrl, getHeroComicsUrl } from '@services/SiteConfig';
+import { getHeroesUrl, getHeroComicsUrl } from '@services/SiteConfig';
 
 export const HeroesContext = createContext();
 
@@ -9,7 +9,6 @@ export const HeroesProvider = ({ children }) => {
   const [heroes, setHeroes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [activeHero, setActiveHero] = useState(null);
   const [favoriteHeroes, setFavoriteHeroes] = useState([]);
   const [showFavorites, setShowFavorites] = useState(false);
 
@@ -58,12 +57,16 @@ export const HeroesProvider = ({ children }) => {
       setLoading(true);
       const url = getHeroComicsUrl(heroId);
       const data = await fetchData(url);
-      //variable comics para ordenar los comics por fecha de lanzamiento
+      // Variable comics para ordenar los comics por fecha de lanzamiento
       const comics = data.data.results;
 
       comics?.sort((a, b) => {
-        const dateA = new Date(a.dates.find(date => date.type === 'onsaleDate').date);
-        const dateB = new Date(b.dates.find(date => date.type === 'onsaleDate').date);
+        const dateA = new Date(
+          a.dates.find((date) => date.type === 'onsaleDate').date
+        );
+        const dateB = new Date(
+          b.dates.find((date) => date.type === 'onsaleDate').date
+        );
         return dateA - dateB;
       });
 
@@ -76,50 +79,12 @@ export const HeroesProvider = ({ children }) => {
     }
   };
 
-  // Función para obtener los detalles de un heroe, guardando los datos en localStorage por cada heroe que se solicite
-  const fetchHeroDetail = async (heroId) => {
-    try {
-      setLoading(true);
-      const now = new Date().getTime();
-      const cacheKey = `hero_${heroId}`;
-      const cachedHeroe = JSON.parse(localStorage.getItem(cacheKey));
-      const lastFetch = localStorage.getItem(`${cacheKey}_lastFetch`);
-
-      /* Si el heroe está guardado en localStorage y
-       * la última vez que se hizo la petición no fue hace más de 24 horas,
-       * retornamos los datos guardados */
-      if (cachedHeroe && lastFetch && now - lastFetch < 24 * 60 * 60 * 1000) {
-        return cachedHeroe;
-      }
-
-      // Si no, hacemos la solicitud y guardamos los datos en localStorage
-      const url = getHeroDetailUrl(heroId);
-      const data = await fetchData(url);
-      const results = data.results;
-
-      localStorage.setItem(cacheKey, JSON.stringify(results));
-      localStorage.setItem(`${cacheKey}_lastFetch`, now);
-
-      return results;
-    } catch (err) {
-      setError('Failed to fetch hero detail:', err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // Función para seleccionar un heroe como activo
-  const selectHero = (hero) => {
-    setActiveHero(hero);
-    localStorage.setItem('activeHero', JSON.stringify(hero));
-  };
-
   // Función para marcar/desmarcar héroes como favoritos
   const toggleFavoriteHero = (hero) => {
     const isFavorite = favoriteHeroes.includes(hero.id);
     let updatedFavorites;
     if (isFavorite) {
-      updatedFavorites = favoriteHeroes.filter(id => id !== hero.id);
+      updatedFavorites = favoriteHeroes.filter((id) => id !== hero.id);
     } else {
       updatedFavorites = [...favoriteHeroes, hero.id];
     }
@@ -130,18 +95,17 @@ export const HeroesProvider = ({ children }) => {
   // Función para filtrar los héroes favoritos
   const filterFavoriteHeroes = () => {
     return heroes.filter((hero) => favoriteHeroes.includes(hero.id));
-  }
+  };
 
   // Función para mostrar/ocultar los héroes favoritos
   const toggleShowFavorites = () => {
     setShowFavorites(!showFavorites);
-  }
+  };
 
   // Función para limpiar el filtro de favoritos
   const clearFavoriteFilter = () => {
     setShowFavorites(false);
-  }
-
+  };
 
   return (
     <HeroesContext.Provider
@@ -149,9 +113,6 @@ export const HeroesProvider = ({ children }) => {
         heroes,
         loading,
         error,
-        activeHero,
-        selectHero,
-        fetchHeroDetail,
         fetchHeroComics,
         toggleFavoriteHero,
         favoriteHeroes,
